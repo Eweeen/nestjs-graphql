bff020bedc5efe5581ef22ffd36d486541b84090
Ewen35550 <ewen.herve.35550@gmail.com>
2024-01-25 15:54:00 +0100

    fix(cors) enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import {NestExpressApplication} from "@nestjs/platform-express";
+import helmet from "helmet";
 
 config({ path: '.env' });
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({ origin: '*',  credentials: true });
+  app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+  app.disable('x-powered-by');
+
   await app.listen(process.env.PORT);
 }
 
